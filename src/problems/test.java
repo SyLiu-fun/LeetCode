50c1feb00d905debda7c0f931f64cb16aeb823f5
lsylsy <1454503452@qq.com>
2022-09-24 12:10:41 +0800

    add a solution for Problem 1652(Defuse the Bomb)

--- src/problems/test.java
+++ src/problems/test.java
@@ -5,12 +5,7 @@ import java.util.Random;
 //问题测试类
 public class test {
     public static void main(String[] args) {
-        LinkedListSimulator lls = new LinkedListSimulator();
-        lls.addAtHead(1);
-        lls.addAtTail(3);
-        lls.addAtIndex(1, 2);
-        lls.get(1);
-        lls.deleteAtIndex(1);
-        lls.get(1);
+        Decrypt d = new Decrypt();
+        System.out.println(-1 % 4);
     }
 }
