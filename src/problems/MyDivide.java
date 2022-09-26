e5a7d66eb0c44dee5daadcc55afb9adac1e536b5
lsylsy <1454503452@qq.com>
2022-09-26 19:00:27 +0800

    add a solution for Problem 29(Divide Two Integers)

--- src/problems/MyDivide.java
+++ src/problems/MyDivide.java
@@ -19,12 +19,18 @@ public class MyDivide {
 
     //被除数是a，除数是b，均为负数
     public int div(int a, int b){
+        //a > b，说明不够除，商为0
         if(a > b) return 0;
+        //a < b,够÷，商至少为1，step记录当前除数的倍数
         int cnt = 1, step = b;
+        //step + step >= a判断被除数是否还能除以step，如果够除，除数翻倍，除数的个数也翻倍
+        //如果step < a/2，说明不能继续翻倍
+        //魔鬼细节，如果写成step + step >= a, 当a为MIN_VALUE时，step + step可能会溢出，换成减法则不会
         while(step >= a - step){
             cnt += cnt;
             step += step;
         }
+        //当前迭代含有cnt个b，余数小于step，大于b，继续递归计算还有多少个b
         return cnt + div(a - step, b);
     }
 
