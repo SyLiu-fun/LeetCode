0be24a98aa337ae9de2d6f8ede65b452645205e6
lsylsy <1454503452@qq.com>
2022-09-27 15:54:34 +0800

    add a solution for Problem 169(Major Element)

--- src/problems/test.java
+++ src/problems/test.java
@@ -5,8 +5,6 @@ import java.util.Random;
 //问题测试类
 public class test {
     public static void main(String[] args) {
-        Game g = new Game();
-        g.ballGame(6, new String[]{"....", ".EE.", "O.E.", "...."});
-        System.out.println();
+
     }
 }
